8b6ed5296b7cb62bf9ed2492fde2d9a0d44b5315
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-01 22:39:08 +0530

    :construction: Test out actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    HttpResponse::Ok().body(format!("Hello, {}!", name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(greet))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }
