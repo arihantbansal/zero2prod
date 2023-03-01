636b680f37c4d64b7371a145fd5913943051aaff
arihantbansal <17180950+arihantbansal@users.noreply.github.com>
2023-03-01 22:42:02 +0530

    :sparkles: implement health_check

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[get("/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    HttpResponse::Ok().body(format!("Hello, {}!", name))
+#[get("/health_check")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello).service(greet))
+    HttpServer::new(|| App::new().service(health_check))
         .bind(("127.0.0.1", 8000))?
         .run()
         .await
